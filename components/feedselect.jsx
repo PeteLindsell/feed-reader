76a85e634d3b71c44430a7f6d81bbc46d3d13844
pete80 <pete.lindsell@gmail.com>
2016-06-14 09:04:31 +0100

    Added array of feeds

--- components/feedselect.jsx
+++ components/feedselect.jsx
@@ -2,19 +2,51 @@ import React from "react";
 
 const FeedSelect = React.createClass ({
   getDefaultProps: function () {
+    // TODO: These should not be hard coded but could not find a list as you would expect from an api.
     return {
       fieldMap: {
+        "popular": [
+          { value: "news", label: "Top Stories" },
+          { value: "news/world", label: "World" },
+          { value: "news/uk", label: "World" },
+          { value: "news/business", label: "Business" },
+          { value: "news/politics", label: "Politics" },
+          { value: "news/health", label: "Health" },
+          { value: "news/education", label: "Education & Family" },
+          { value: "news/science_and_environment", label: "Science & Environment" },
+          { value: "news/technology", label: "Technology" },
+          { value: "news/entertainment_and_arts", label: "Entertainment & Arts" }
+        ],
         "global": [
           { value: "news/world/africa", label: "Africa" },
-          { value: "news/world/asia", label: "Asia" }
+          { value: "news/world/asia", label: "Asia" },
+          { value: "news/world/europe", label: "Europe" },
+          { value: "news/world/latin_america", label: "Latin America" },
+          { value: "news/world/middle_east", label: "Middle East" },
+          { value: "news/world/us_and_canada", label: "US & Canada" },
+          { value: "news/world/england", label: "England" },
+          { value: "news/world/northern_ireland", label: "Northern Ireland" },
+          { value: "news/world/scotland", label: "Scotland" },
+          { value: "news/world/wales", label: "Wales" }
         ],
         "video": [
           { value: "news/video_and_audio/news_front_page", label: "Top stories" },
-          { value: "news/video_and_audio/world", label: "World" }
+          { value: "news/video_and_audio/world", label: "World" },
+          { value: "news/video_and_audio/uk", label: "UK" },
+          { value: "news/video_and_audio/business", label: "Business" },
+          { value: "news/video_and_audio/politics", label: "Politics" },
+          { value: "news/video_and_audio/health", label: "Health" },
+          { value: "news/video_and_audio/science_and_environment", label: "Science & Environment" },
+          { value: "news/video_and_audio/technology", label: "Technology" }
         ],
         "other": [
-          { value: "news/world/africa", label: "Africa" },
-          { value: "news/world/asia", label: "Asia" }
+          { value: "news/system/latest_published_content/rss.xml", label: "Latest published stories" },
+          { value: "news/magazine", label: "Magazine" },
+          { value: "news/also_in_the_news", label: "Also in the news" },
+          { value: "rss/newsonline_uk_edition/in_pictures", label: "In Pictures" },
+          { value: "news/special_reports", label: "Special Reports" },
+          { value: "news/theeditors", label: "Have your say" },
+          { value: "news/editors_blog", label: "Editors Blog" }
         ]
       }
     }
@@ -65,9 +97,10 @@ const FeedSelect = React.createClass ({
     return (
     <div>
       <select className="form-control" onChange={this.handleFirstLevelChange} value={this.state.firstValue}>
+        <option value="popular">Popular BBC News Feeds</option>
         <option value="global">Global and UK News Feeds</option>
-         <option value="video">Video & Audio News Feeds</option>
-         <option value="other">Other News Feeds</option>
+        <option value="video">Video & Audio News Feeds</option>
+        <option value="other">Other News Feeds</option>
        </select>
       {this.getSecondLevelField()}
     </div>
--- output/app.js
+++ output/app.js
@@ -174,11 +174,13 @@ var FeedSelect = _react2.default.createClass({
   displayName: "FeedSelect",
 
   getDefaultProps: function getDefaultProps() {
+    // TODO: These should not be hard coded but could not find a list as you would expect from an api.
     return {
       fieldMap: {
-        "global": [{ value: "news/world/africa", label: "Africa" }, { value: "news/world/asia", label: "Asia" }],
-        "video": [{ value: "news/video_and_audio/news_front_page", label: "Top stories" }, { value: "news/video_and_audio/world", label: "World" }],
-        "other": [{ value: "news/world/africa", label: "Africa" }, { value: "news/world/asia", label: "Asia" }]
+        "popular": [{ value: "news", label: "Top Stories" }, { value: "news/world", label: "World" }, { value: "news/uk", label: "World" }, { value: "news/business", label: "Business" }, { value: "news/politics", label: "Politics" }, { value: "news/health", label: "Health" }, { value: "news/education", label: "Education & Family" }, { value: "news/science_and_environment", label: "Science & Environment" }, { value: "news/technology", label: "Technology" }, { value: "news/entertainment_and_arts", label: "Entertainment & Arts" }],
+        "global": [{ value: "news/world/africa", label: "Africa" }, { value: "news/world/asia", label: "Asia" }, { value: "news/world/europe", label: "Europe" }, { value: "news/world/latin_america", label: "Latin America" }, { value: "news/world/middle_east", label: "Middle East" }, { value: "news/world/us_and_canada", label: "US & Canada" }, { value: "news/world/england", label: "England" }, { value: "news/world/northern_ireland", label: "Northern Ireland" }, { value: "news/world/scotland", label: "Scotland" }, { value: "news/world/wales", label: "Wales" }],
+        "video": [{ value: "news/video_and_audio/news_front_page", label: "Top stories" }, { value: "news/video_and_audio/world", label: "World" }, { value: "news/video_and_audio/uk", label: "UK" }, { value: "news/video_and_audio/business", label: "Business" }, { value: "news/video_and_audio/politics", label: "Politics" }, { value: "news/video_and_audio/health", label: "Health" }, { value: "news/video_and_audio/science_and_environment", label: "Science & Environment" }, { value: "news/video_and_audio/technology", label: "Technology" }],
+        "other": [{ value: "news/system/latest_published_content/rss.xml", label: "Latest published stories" }, { value: "news/magazine", label: "Magazine" }, { value: "news/also_in_the_news", label: "Also in the news" }, { value: "rss/newsonline_uk_edition/in_pictures", label: "In Pictures" }, { value: "news/special_reports", label: "Special Reports" }, { value: "news/theeditors", label: "Have your say" }, { value: "news/editors_blog", label: "Editors Blog" }]
       }
     };
   },
@@ -239,6 +241,11 @@ var FeedSelect = _react2.default.createClass({
       _react2.default.createElement(
         "select",
         { className: "form-control", onChange: this.handleFirstLevelChange, value: this.state.firstValue },
+        _react2.default.createElement(
+          "option",
+          { value: "popular" },
+          "Popular BBC News Feeds"
+        ),
         _react2.default.createElement(
           "option",
           { value: "global" },
